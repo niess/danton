8568f1214f93d7a2ec913ffc3aebcc9972c5067b
Valentin Niess <niess@in2p3.fr>
2024-07-26 09:10:19 +0200

    Backward example

---
+++ examples/backward.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+import danton
+
+# Create a Danton simulation interface.
+simulation = danton.Simulation()
+
+# Configure the Monte Carlo for backward sampling of tau decays.
+simulation.mode = "backward"
+simulation.tau_decays = True
+
+# Set an elliptical Earth (using the WGS84 ellipsoid).
+simulation.geometry.geodesic = "WGS84"
+
+# Generate a bunch of taus at decay, with an elevation angle of 1 deg.
+particles = danton.particles(10, energy=1E+09, elevation=1.0)
+
+# Run the Monte Carlo simulation.
+result = simulation.run(particles)
+
+# Inspect the results.
+print(result)
--- examples/forward.py
+++ examples/forward.py
@@ -1,9 +1,24 @@
 #! /usr/bin/env python3
 import danton
 
+# Create a Danton simulation interface.
 simulation = danton.Simulation()
+
+# Configure the Monte Carlo for forward flux sampling (using the longitudinal
+# approximlation).
 simulation.mode = "forward"
+simulation.tau_decays = False
+simulation.longitudinal = True
+
+# Set a spherical Earth (using PREM model).
 simulation.geometry.geodesic = "PREM"
+
+# Generate a bunch of neutrinos (that would emerge with an elevation angle of 1
+# deg at the equator).
 particles = danton.particles(100, pid=16, energy=1E+10, elevation=1.0)
+
+# Run the Monte Carlo simulation.
 result = simulation.run(particles)
+
+# Inspect the results.
 print(result)
