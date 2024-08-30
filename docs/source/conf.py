ec1755be2b9b0f2b910474fb6b394c92cf41fa8b
Valentin Niess <niess@in2p3.fr>
2024-08-30 14:49:17 +0200

    Bump version: v1.3.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ except ModuleNotFoundError:
 project = "Danton"
 copyright = "Université Clermont Auvergne, CNRS/IN2P3, LPCA"
 author = "Valentin Niess"
-release = "1.2.0"
+release = "1.3.0"
 
 highlight_language = "python3"
 
--- src/python/danton/__init__.py
+++ src/python/danton/__init__.py
@@ -1,4 +1,4 @@
 from .danton import *
 del danton
 
-VERSION = "1.2.0"
+VERSION = "1.3.0"
